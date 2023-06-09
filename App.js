cd577643a474c4c6a4d97832fd888e1dcbf2bfd4
OkolieOKS234 <dokolie040@gmail.com>
2023-06-09 13:57:49 +0100

    app basics

---
+++ App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text className="text-lg text-red-700 text-center flex justify-center items-center my-80">code with OK$</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# FOOD DELIVERY APP REMODIFIED
+
+## LIBRARIES OR TECH STACK
+- expo
+- react native
+- native wind(equivalent to tailwind css)
+- Sanity.io
+- Redux
