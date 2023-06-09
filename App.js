79e343176e13208c9dec045567ce1311b413aeaa
OkolieOKS234 <dokolie040@gmail.com>
2023-06-09 14:13:06 +0100

    created a navigation file

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 
 export default function App() {
   return (
